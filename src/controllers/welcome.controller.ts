0e4073f8c7f5883145eb62a43d678486c5af027c
KimMachineGun <geon0250@gmail.com>
2019-02-08 20:14:26 +0900

    JSON 파싱 로직 추가

--- src/controllers/welcome.controller.ts
+++ src/controllers/welcome.controller.ts
@@ -6,6 +6,29 @@ router.get("/", (req: Request, res: Response) => {
   res.send("Hello world\n");
 });
 
+class Person {
+  public static fromJsonObj({name, age}: {name: string, age: number}): Person {
+    return new Person(name, age);
+  }
+
+  public name: string;
+  public age: number;
+
+   constructor(name: string, age: number = 0) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public greet(): string {
+    return `Hello my name is ${this.name}. I'm ${this.age} year-old.`;
+  }
+}
+
+router.get("/json", (req: Request, res: Response) => {
+  const p: Person = Person.fromJsonObj(req.body);
+  res.send(`MSG: ${p.greet()}\n`);
+});
+
 router.get("/:name", (req: Request, res: Response) => {
   const { name } = req.params;
   res.send(`Hello, ${name}\n`);
